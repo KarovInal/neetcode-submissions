3875be5fd33953159a83691d9a332231c280b628
KarovInal <inal.karov8@gmail.com>
2026-09-19 17:40:53 +0300

    Add: balanced-binary-tree - submission-13

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-13.ts
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {boolean}
+     */
+    isBalanced(root: TreeNode | null): boolean {
+        let isBalanced = true;
+
+        function inorder(root: TreeNode | null) {
+            if(!root) return { lh: -1, rh: -1 };
+
+            const l = inorder(root.left);
+            let lh = 1 + Math.max(l.lh, l.rh);
+            const r = inorder(root.right);
+            let rh = 1 + Math.max(r.lh, r.rh);
+
+            if(Math.abs(lh-rh) > 1) {
+                console.log("root", root);
+                console.log("lh", lh, root.left);
+                console.log("lr", rh, root.right);
+                isBalanced = false;
+            }
+
+            return { lh, rh };
+        }
+
+        inorder(root);
+
+        return isBalanced;
+    }
+}
